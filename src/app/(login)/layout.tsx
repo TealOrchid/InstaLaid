6b59b54a012cf62ef5e8950142fb4f64beac4738
TealOrchid <tealorchid@proton.me>
2024-12-05 23:59:55 +0100

    Do pandas like sparkling water?

--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -1,10 +1,16 @@
 import { ReactNode } from 'react';
 import "../globals.css";
 import { SessionProvider } from 'next-auth/react';
+import { Metadata } from 'next';
 
 interface RootLayoutProps {
   children: ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: "InstaLaid",
+  description: "Pistachios are fucking disgusting!",
+};
 
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
